683a62a392b9631fbb9551007339076a24d5a7d8
Anish Vishwanathan <anish@zovi.com>
2015-12-03 19:16:22 +0530

    post

--- js/list.js
+++ js/list.js
@@ -2,16 +2,61 @@
 var app = angular.module("HelpChennaiApp", []);
 
 app.controller('HelpChennaiController', function($scope, $http) {
-	
-		$http({
-            method: 'GET',
-            url: '/api/search?lon=0&lat=0'
-        })
-        .success(function(data, status) {
-        	$scope.listing = data.data;
+		$scope.init =function(){
+			$scope.listmode = true;
+			$scope.postdata = {
+				title: "",
+				name: "",
+				phone: "",
+				address: "",
+				locality: "",
+				tags: []
+
+			}
+			$scope.tags = {"shelter" : false, "food" : false, "medicine": false, "recharge": false}
+			$scope.load_data()
+		}
+		$scope.load_data = function(){
+			$http({
+	            method: 'GET',
+	            url: '/api/search?lon=0&lat=0'
+	        })
+	        .success(function(data, status) {
+	        	$scope.listing = data.data;
+
+	        });
+
+		}
+			
+        $scope.addListing = function() {
+        	$scope.listmode = false;
+        }
+
+        $scope.populateTags = function(t, v) {
+        	$scope.tags[t] = v;
+
+        }
+
+        $scope.post = function(param) {
         	
-        })
-	
+
+
+        	for(var key in $scope.tags) {
+        		if($scope.tags[key] == true){
+        			$scope.postdata.tags.push(key);
+        		}
+        			
+        	}
+        	$http({
+        	  method  : 'POST',
+        	  url     : 'api/post',
+        	  data    : angular.toJson($scope.postdata),  // pass in data as strings
+        	  headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+        	 })
+        	  .success(function(data) {
+        	  });
+        }
+		$scope.init();
 })
 
 
